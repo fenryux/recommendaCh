0a7e3a18249995d0b984170cac33b30b510bf3be
fenryux <vlmkovalenko@gmail.com>
2023-04-02 17:12:51 +0300

    govno

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,17 @@
-<script setup>
-import { RouterLink, RouterView } from 'vue-router';
+<script>
+import { RouterView } from 'vue-router';
+import Navbar from './components/Navbar.vue';
+export default{
+  components: {
+    Navbar
+  }
+}
 </script>
 
 <template>
-  <header>
-    <div class="wrapper">
-      <nav>
-        <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/about">About</RouterLink>
-      </nav>
-    </div>
-  </header>
-
-  <RouterView />
+  <Navbar />
+  <div>
+    <RouterView />
+  </div>
 </template>
 
---
+++ src/components/Navbar.vue
@@ -0,0 +1,15 @@
+<template>
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200" />
+
+</template>
+
+<style>
+    @import url('https://fonts.googleapis.com/css2?family=Roboto&display=swap');
+    .material-symbols-outlined {
+        font-variation-settings:
+        'FILL' 0,
+        'wght' 400,
+        'GRAD' 0,
+        'opsz' 48
+    }
+</style>
