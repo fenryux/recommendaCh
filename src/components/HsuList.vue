8d1df1f9e526d8c5bd16c8da27018e69c18594f7
Марк Бобровских <markbobrovskih@MacBook-Pro-Mark.local>
2023-04-04 18:44:37 +0300

    test component created

---
+++ src/components/HsuList.vue
@@ -0,0 +1,30 @@
+<template>
+  <div>
+    <v-expansion-panels class="mb-6">
+      <v-expansion-panel>
+        <v-expansion-panel-title expand-icon="mdi-plus" collapse-icon="mdi-minus">
+          Item
+        </v-expansion-panel-title>
+        <v-expansion-panel-text>
+          test text
+        </v-expansion-panel-text>
+      </v-expansion-panel>
+
+      <v-expansion-panel>
+        <v-expansion-panel-title expand-icon="mdi-plus" collapse-icon="mdi-minus">
+          Item
+        </v-expansion-panel-title>
+        <v-expansion-panel-text>
+          test text
+        </v-expansion-panel-text>
+      </v-expansion-panel> <v-expansion-panel>
+      <v-expansion-panel-title expand-icon="mdi-plus" collapse-icon="mdi-minus">
+        Item
+      </v-expansion-panel-title>
+      <v-expansion-panel-text>
+        test text
+      </v-expansion-panel-text>
+    </v-expansion-panel>
+    </v-expansion-panels>
+  </div>
+</template>
