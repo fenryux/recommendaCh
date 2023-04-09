6dbb3faa7a8fb354fda7577ce8a6a0d90a435c2d
xxone-a-i <mark0820112@gmail.com>
2023-04-09 21:14:41 +0300

    Test page

--- src/components/HsuList.vue
+++ src/components/HsuList.vue
@@ -3,10 +3,11 @@
     <v-expansion-panels class="mb-6">
       <v-expansion-panel>
         <v-expansion-panel-title expand-icon="mdi-plus" collapse-icon="mdi-minus">
-          Item
+          <v-icon class="icon">$expand</v-icon>
+          МГТУ "СТАНКИН"
         </v-expansion-panel-title>
         <v-expansion-panel-text>
-          test text
+          Московский государственный универститет "СТАНКИН"
         </v-expansion-panel-text>
       </v-expansion-panel>
 
