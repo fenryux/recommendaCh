f5569dab156fad19329a242563669d7c9998bc0d
fenryux <vlmkovalenko@gmail.com>
2023-04-09 19:46:53 +0300

    navbar fixed, signin and signup views

--- src/router/index.js
+++ src/router/index.js
@@ -6,7 +6,7 @@ const router = createRouter({
     history: createWebHistory(import.meta.env.BASE_URL),
     routes: [
         {
-            path: '/',
+            path: '/sign-in',
             name: 'sign-in',
             component: SignInView
         },
