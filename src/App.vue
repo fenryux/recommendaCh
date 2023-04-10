a14367b10b82afc61b9195efc092bb891bdbc2bd
xxone-a-i <mark0820112@gmail.com>
2023-04-10 08:03:20 +0300

    last commit

--- src/App.vue
+++ src/App.vue
@@ -2,13 +2,14 @@
 import { RouterView } from 'vue-router';
 import Navbar from './components/Navbar.vue';
 import Footer from './components/Footer.vue';
+import HsuList from "@/components/HsuList.vue";
 </script>
 
 <template>
   <v-app class="grey lighten-4">
     <Navbar />
     <v-spacer></v-spacer>
-    <RouterView />
+    <RouterView></RouterView>
     <v-spacer></v-spacer>
     <Footer />
   </v-app>
